93061b43decadc160bd3f6593b9757c77050c9dd
Andrew Barba <abarba.77@gmail.com>
2016-02-10 22:59:05 -0500

    lab 5 pre-lab

---
+++ lab_5/CreateDir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+DIR_NAME="${1}"
+
+mkdir $DIR_NAME
---
+++ lab_5/ShowDate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT_DATE=$(date)
+
+printf "The current date is ${CURRENT_DATE}\n"
+
+#### Bad code...
+#echo -n "The current date is "
+#date
